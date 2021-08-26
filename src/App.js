e2a8266bffdb5635c5ec759c7820978e7912e340
Carlito <rody.castillo.tacuri@gmail.com>
2021-08-25 23:14:50 -0500

    Add new data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { AddCat } from './components/AddCat';
 
-function App() {
+const App = () => {
+  const [categories, setCategories] = useState(['One Piece', 'Death Note', 'Bleach'])
+
+  // const handleAdd = () => {
+  //   //Mutar
+  //   // return setCategories(categories.concat('Hola'))
+  //   return setCategories([...categories, 'Hunter'])
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>GiffExpertApp</h2>
+      <AddCat setCategories={ setCategories }/>
+      <hr />
+      {/* <button onClick={handleAdd}>Agregar</button> */}
+      <ol>
+        {categories.map(cat => {
+          return <li> {cat} </li>
+        })}
+      </ol>
+    </>
   );
 }
 
---
+++ src/components/AddCat.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+export const AddCat = ({ setCategories }) => {
+
+    const [inputValue, setInputValue] = useState('')
+
+    const handleInputChange = (e) => {
+        return setInputValue(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(inputValue.trim().length > 2) {
+            setCategories( cats => [...cats, inputValue])
+            setInputValue('')
+        }
+    }
+    return (
+        <form onSubmit={ handleSubmit }>
+            <h1>{ inputValue }</h1>
+            <input
+                type="text"
+                value= { inputValue }
+                onChange={ handleInputChange }
+            />
+        </form>
+    )
+}
+
+AddCat.propTypes= {
+    setCategories: PropTypes.func.isRequired
+}
